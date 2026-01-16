3795d84c7ebd1738437bd20863ebdc03965d3f08
Krishna Bokefod <87262925+krishna976762@users.noreply.github.com>
2026-01-16 15:52:18 +0530

    change key

--- src/utils/OpenAi.js
+++ src/utils/OpenAi.js
@@ -2,7 +2,7 @@ import OpenAI from 'openai';
 import { OPENAPI } from './Constants';
 
 const openai = new OpenAI({
-  apiKey: OPENAPI, 
+  apiKey: "AIzaSyBVlBVccyRbtWVz5z5DLpFjQMzu4gAW1b8", 
   dangerouslyAllowBrowser:true
 }); 
 
